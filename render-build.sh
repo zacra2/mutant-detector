0c94dcc316564a04e6b331fb7564b8aa73e6933e
zacra2 <lali_yanina_michref@hotmail.com>
2024-11-02 23:22:07 -0300

    ajustes

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-
-# Asegúrate de que JAVA_HOME esté configurado
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
-export PATH=$JAVA_HOME/bin:$PATH
-
-# Compilar tu aplicación
-./gradlew build
+npm install
+npm run build./render-build.sh
